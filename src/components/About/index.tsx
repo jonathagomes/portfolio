3dcd6dd91509a9cc4176e0e73bfd6878384ecf30
Jonatha Gomes <jonatha.dev.js@gmail.com>
2021-06-30 12:36:58 -0300

    Changing the Curriculum URL

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,30 +1,38 @@
-import { 
- AboutContainer,
- AboutContent,
- Title,
- TextRed
-} from "./styles";
+import { AboutContainer, AboutContent, Title, TextRed } from "./styles";
 
-export default function About () {
- return(
-  <AboutContainer id="about">
-   <AboutContent>
-    <div>
-     <img src="/react.svg"/>
-    </div>
-    <div>
-      <Title>
-       <h1>QUEM SOU EU?</h1>
-      </Title>
-      <p>Hello, meu nome é Jonatha Gomes</p>
-      <p>Sou um <TextRed>Desenvolvedor Fullstack Javascript</TextRed></p>
-      <p>Em geral, gosto de gerar novas soluções para 
-       problemas.<br/>Sou esforçado, motivado e adoro desafios.<br/>
-       Estou focado na <TextRed>Ecossistema Javascript</TextRed>, porém quero<br/>
-       focar em <TextRed>NodeJS, ReactJS e React Native</TextRed>.</p>
-       <a href="https://drive.google.com/file/d/162ampawMe1ZkkbL5OzdtL7dP2dOEZ2m-/view" target="_blank">VER MEU CURRÍCULO</a>
-     </div> 
-   </AboutContent>
-  </AboutContainer>
- );
+export default function About() {
+  return (
+    <AboutContainer id="about">
+      <AboutContent>
+        <div>
+          <img src="/react.svg" />
+        </div>
+        <div>
+          <Title>
+            <h1>QUEM SOU EU?</h1>
+          </Title>
+          <p>Hello, meu nome é Jonatha Gomes</p>
+          <p>
+            Sou um <TextRed>Desenvolvedor Fullstack Javascript</TextRed>
+          </p>
+          <p>
+            Em geral, gosto de gerar novas soluções para problemas.
+            <br />
+            Sou esforçado, motivado e adoro desafios.
+            <br />
+            Estou focado na <TextRed>Ecossistema Javascript</TextRed>, porém
+            quero
+            <br />
+            focar em <TextRed>NodeJS, ReactJS e React Native</TextRed>.
+          </p>
+          <a
+            href="https://drive.google.com/file/d/10KTUkm_wYfaJuNuG-U74c8k0s0xkjoO8/view?usp=sharing"
+            target="_blank"
+          >
+            VER MEU CURRÍCULO
+          </a>
+        </div>
+      </AboutContent>
+    </AboutContainer>
+  );
 }
